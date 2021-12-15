048c4ae826cd2b2c00db924457eae8bfb22e9321
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2021-12-15 12:14:03 +0100

    Commenting my code

--- Exo_1.py
+++ Exo_1.py
@@ -7,26 +7,31 @@ DAYS = {'1': 'Lundi', '2':'Mardi', '3': 'Mercredi', '4': 'Jeudi', '5': 'Vendredi
 
 def get_date(date_str):
     ''' 
+    date_str: la date de créneau impossible 
 
+    >> Split le créneau pour un employeur
+
+    Example:
     Input : '2 08:39-09:48'
     Output:  (2, 08:39, 09:48)
-    
     '''
     aux_split = date_str.split()
-    day = aux_split[0]
-    slot = aux_split[1]
+    day, slot = aux_split[0], aux_split[1]
     aux_indispo = aux_split[1].split('-')
-    slotStart = aux_indispo[0]
-    slotEnd = aux_indispo[1]
+    slotStart, slotEnd = aux_indispo[0], aux_indispo[1]
     return day, slotStart, slotEnd
 
 
 def get_days(N, len_N):
     '''
+    N: la liste des créneaux impossibles 
+    len_N: la taille de N
 
+    >> Extraire les jours existant dans la liste N
+
+    Example :
     Input: ['6', '2 08:39-09:48', '2 08:12-11:08', '1 13:09-16:27', '4 15:18-15:23', '3 14:05-17:51', '2 13:19-17:18'], 6
     Output: ['1', '2', '3', '4']
-
     '''
     N_days = []
     for i in range(1, len_N):
@@ -37,28 +42,38 @@ def get_days(N, len_N):
 
 
 def get_slot_day(N, len_N, day):
-    '''
+    ''' 
+    N: la liste des créneaux impossibles 
+    len_N: la taille de N
+    day: jour
 
-    Input: N, la taille de N, day >> (N, 6, '1')
+    >> Je fixe un jour x pour que je puisse récupérer les créneaux impossibles juste de ce jour.
+
+    Example:
+    Input:  (N, 6, '1')
     Output: ['08:00', '13:09', '16:27', '17:59'] 
 
     '''
     slot_day = []
-    # slot_day.append('08:00')
+
     for i in range(1, len_N+1):
         day_aux , slotStart, slotEnd = get_date(N[i])
         # print(get_date(N[i]))
         if day_aux == day :
             slot_day.append([slotStart, slotEnd])
-    # slot_day.append('17:59')
+
+    # Je trie suivant le 'slotStart' pour évier le cas 1
     slot_day = sorted(slot_day)
 
     # print(slot_day)
     flat_list = [item for sublist in slot_day for item in sublist]
 
     for i in range(1, len(flat_list)-1, 2):
+
+        # Pour éviter le problème du cas 2
         if(flat_list[i]>flat_list[i+2]):
             flat_list[i], flat_list[i+2] = flat_list[i+2], flat_list[i]
+
     flat_list.insert(0, '08:00')
     flat_list.append('17:59')
     return flat_list
@@ -66,36 +81,48 @@ def get_slot_day(N, len_N, day):
 
 def calculate_difference_time(startTime, endTime):
     '''
+    startTime: début de créneau 
+    endTime: Fin de créneau
 
+    >>  Faire la soustraction 
+
+    Example:
     Input: 08:00, 08:13
     Output:  14 minutes de différance
-
     '''
+    # J'ai ajouté 1 au résultat pour avoir le nombre exacte de minutes 
     result =  (dt.datetime.strptime(endTime, "%H:%M") - dt.datetime.strptime(startTime, "%H:%M")).total_seconds()/60+1
     # print(result)
     return result
 
 
+
 def check_duration(slot_day):
     '''
-    
+    slot_day: le créneau pour le jour x 
+
+    >> Je vérifie les disponibilité des créneaux pour le jour x.
+    >> Je retourne le créneau de réunion sinon je retourne False.
+
+    Example:
     Input: les créneaux impossibles >> ['08:00', '13:09', '16:27', '17:59']
     Output: Horaire de réunion (08:00,08:59)
-
     '''
-    # slot_day = get_slot_day(N, day)
     len_slot = len(slot_day)
     # print(slot_day)
+
     for i in range(0, len_slot, 2) :
         diff = calculate_difference_time(slot_day[i], slot_day[i+1])
         # print(slot_day[i], diff)
         if diff >= 60:
+            # Je retourne le créneau de réunion s'il vérifie les contraintes 
             return slot_founded(slot_day[i])
             # print("Starting", slot_day[i])
             # break
-    # return False
+    return False
 
 def slot_founded(startTime):
+    # J'ajoute juste 59 mn pour avoir le nombre exacte des minutes
     endTime = dt.datetime.strptime(startTime, "%H:%M") + dt.timedelta(minutes= 59)
     endTime = format(endTime, '%H:%M')
     # print(endTime)
@@ -108,12 +135,7 @@ def main():
     N_days = get_days(N, len_N)
 
 
-    # print(calculate_difference_time('08:00', '08:13'))
-    # print(N)
     for day in N_days:
-        # print(day)
-        # print(get_slot_day(N, len_N, day))
-        # print(f'Daaaaaaaaaaaaaaaay >> {day}')
         slot_duration = get_slot_day(N, len_N, day)
         founded_slot = check_duration(slot_duration)
         if(founded_slot):
@@ -121,7 +143,6 @@ def main():
             # print(f'Your slot is: {DAYS[day]} : {founded_slot[0]} >> {founded_slot[1]}')
             return 0
     print('Slot not founded /!\\')
-    # slot_founded('09:20')
 
     
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GAC_Technology
+
+
+###  Les cas que j'ai traité ! 
+
+![IMG_20211215_120442](https://user-images.githubusercontent.com/46791116/146176492-468eecd9-f388-4c9f-8825-bbff2edfc529.jpg)
